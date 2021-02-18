d5f5de2c80215a62460867e031a8c1e1b6e21954
George Dilthey <george.dilthey@gmail.com>
2021-02-18 10:52:47 -0500

    commit tree 99963c7a908487fb2da5d5c09af3464a36c5f302

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+
+require 'pry'
+
+class School
+
+    attr_reader :roster
+    
+    def initialize(name, roster = {})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        
+        else
+        @roster[grade] = []
+        @roster[grade] << name
+
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster={}
+        @roster.each {|key, value| sorted_roster[key] = value.sort}
+        sorted_roster
+    end
+end
+
+# mtg = School.new("Mount Greylock")
+# mtg.add_student("George",12)
+# mtg.add_student("Emily",12)
